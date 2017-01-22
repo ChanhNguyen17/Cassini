8dc7f82585b5fa7250231b32aaa695ed85154820
Chanh Nguyen <chanhnguyen@Chanh-Nguyens-iMac.local>
2017-01-22 23:03:37 +0200

    Remove segue,  add showImage button handler

--- Cassini/CassiniViewController.swift
+++ Cassini/CassiniViewController.swift
@@ -24,6 +24,17 @@ class CassiniViewController: UIViewController, UISplitViewControllerDelegate {
         }
     }
     
+    @IBAction func showImage(_ sender: UIButton) {
+        if let ivc = splitViewController?.viewControllers.last?.contentViewController as? ImageViewController {
+            let imageName = sender.currentTitle
+            ivc.imageURL = DemoURL.NASAImageNamed(imageName) as NSURL?
+            ivc.title = imageName
+        } else {
+            performSegue(withIdentifier: Storyboard.ShowImageSegue,
+                         sender: sender)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         splitViewController?.delegate = self
